c4b4a404f0e97dbfad2ae97f8d4bf565a1750f9d
Lars Brandt <zebrajaeger@zebrajaeger.de>
2021-04-19 16:07:55 +0200

    fix preview image

--- core/src/main/java/de/zebrajaeger/sphere2cube/CubeMapImage.java
+++ core/src/main/java/de/zebrajaeger/sphere2cube/CubeMapImage.java
@@ -44,7 +44,7 @@ public class CubeMapImage implements ReadableImage {
             case LEFT -> new FaceImage(this, false, false, cubeEdge, cubeEdge);
             case FRONT -> new FaceImage(this, false, false, cubeEdge * 2, cubeEdge);
             case RIGHT -> new FaceImage(this, false, false, cubeEdge * 3, cubeEdge);
-            case UP -> new FaceImage(this, false, true, cubeEdge * 2, 0);
+            case UP -> new FaceImage(this, false, false, cubeEdge * 2, 0);
             case DOWN -> new FaceImage(this, false, false, cubeEdge * 2, cubeEdge * 2);
         };
     }
